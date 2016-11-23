5bca9153ba8f6a6bf93b00ec67887cc42e96572e
Austin Huang <austinhuang0131@gmail.com>
2016-11-23 15:53:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ var list = [];
 var result = false;
 
 module.exports = function(query) {
-  rest('http://discord.shoutwiki.com/wiki/Ban_List').then(function(response) {
+	rest('http://discord.shoutwiki.com/wiki/Ban_List').then(function(response) {
 		var templist = response.entity.split('<table class="wikitable">')[1].split('<td>').join('').split('</td>');
 		if (templist.length === 0) {
-			console.log(cWarn("WARN")+" Failed to ping Ban List.");
-			return;
+			return "Failed to ping the Ban List.";
 		}
 		templist.forEach(function(item, index){
 			if(!isNaN(Number(item))){
 				list.push(String(Number(item)));
 			}
 		});
-		fs.writeFile("./Metagon/Metagon/config/banlist.json", list);
 	});
-  if (list.indexOf(query) > -1) {result = true;}
-  return result;
+ 	if (list.indexOf(query) > -1) {result = true;}
+	return result;
 };
