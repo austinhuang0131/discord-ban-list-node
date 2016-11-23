f04c68fcc05ff6ece9fc3735d7806c5e87b1ed44
Austin Huang <austinhuang0131@gmail.com>
2016-11-23 16:49:18 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-var rest = require('rest');
-var list = [];
+var request = require('request');
 var result = false;
+var list = [];
+
+request('http://discord.shoutwiki.com/w/api.php?action=query&titles=Ban%20List&prop=revisions&rvlimit=1&rvprop=content&format=json', function(error, response, body) {
+	if (error || response.statusCode !== 200) {
+		return "Failed to ping the Ban List.";
+	}
+	body = JSON.parse(body);
+	var w = body.query.pages;
+	var body1 = w[Object.keys(w)[0]].revisions[0];
+	var templist = body1[Object.keys(body1)[2]].split("{| class='wikitable'\n|-\n! Username\n! User ID\n! Action\n! Proof\n")[1].split('\n|');
+	if (templist.length === 0) {
+		return "Failed to ping the Ban List.";
+	}
+	templist.forEach(function(entry) {
+		if (entry.startsWith(" ")) {
+			var stuff = entry.split(" ")[1];
+			if (!isNaN(Number(stuff))) {
+				list.push(stuff);
+			}
+		}
+	});
+});
 
 module.exports = function(query) {
-	rest('http://discord.shoutwiki.com/wiki/Ban_List').then(function(response) {
-		var templist = response.entity.split('<table class="wikitable">')[1].split('<td>').join('').split('</td>');
-		if (templist.length === 0) {
-			return "Failed to ping the Ban List.";
-		}
-		templist.forEach(function(item, index){
-			if(!isNaN(Number(item))){
-				list.push(String(Number(item)));
-			}
-		});
-	});
- 	if (list.indexOf(query) > -1) {result = true;}
+	if (list.indexOf(query) > -1) {result = true;}
 	return result;
 };
